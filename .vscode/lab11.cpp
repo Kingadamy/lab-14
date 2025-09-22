25c2b348cbe91ae4b03fc678c089a2e5cf96eb9a
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-09-21 21:03:39 -0700

    update

--- .vscode/lab11.cpp
+++ .vscode/lab11.cpp
@@ -11,7 +11,22 @@ struct MenuItem {
 };
 
 // need to initialize menu item
-   MenuItem(const string& itemName, double itemPrice, int itemQty) 
+   MenuItem(const string& itemName, double itemPrice, int itemQty) {
         : name(itemName), price(itemPrice), quantity(itemQty) {}
 };
 
+// customer order
+struct Order {
+    int tableNumber;
+    vector<MenuItem> items;
+    double totalAmount;
+    bool isPaid;
+
+    Order(int table) : tableNumber(table), totalAmount(0.0), isPaid(false) {}
+
+    //add item to order
+    void addItem(const string& itemName, double price, int quantity) {
+         items.push_back(MenuItem(itemName, price, quantity));
+        totalPrice += price * quantity;
+    }
+}
