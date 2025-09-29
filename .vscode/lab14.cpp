de387dec6ad6bc521ee70034d34ae13d40c1cbbc
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-09-28 19:04:47 -0700

    update

--- .vscode/lab14.cpp
+++ .vscode/lab14.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
+// create the class here
 class color {
 private:
     int red;
     int green;
     int blue;
 
+    // now we must call for these using getters and setters
     public:
     int getRed() { return red; }
     void setRed(int r) { red = r; }
@@ -18,3 +22,11 @@ private:
     int getBlue() { return blue; }
     void setBlue(int b) { blue = b; }
 };
+
+// this is too print the values
+
+void print() {
+    cout << setw(W15) << "Red: " << red << endl;
+    cout << setw(W15) << "Green: " << green << endl;
+    cout << setw(W15) << "Blue: " << blue << endl;
+}
